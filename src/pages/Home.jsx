1411e13cf383058b0b146fa021e62c172a63c34b
Lluis Espert Cuquerella <lluisespertc@gmail.com>
2026-04-02 10:12:29 +0200

    Estructura de React Hecha y funcionando

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../estilos/estilos.css'
+
+const Home = () => {
+
+    return (
+        <div>
+            <h1 className="bienvenida-titulo">¡Bienvenido a tu Agenda!</h1>
+        </div>
+    );
+}
+
+export default Home;
